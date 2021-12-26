60b115b46d6a06649569758c63e16b0e752ccfca
devlunch4@E470 <devlunch4@gmail.com>
2021-12-26 21:48:41 +0900

    initial

---
+++ ch01/ch1-1.js
@@ -0,0 +1,11 @@
+// http module load - http 모듈 로드
+var http = require('http');
+
+// http server create - http 서버 생성
+http.createServer(function (req, res) {
+    // content Header - 컨텐츠 헤ej
+    res.writable(200, { 'content-type': 'text/pain' });
+    // Write a message and send a signal that communication is complete - 메시지를 쓰고 통신이 완료되었다는 신호를 보냄
+    res.end("Hello, World!\n");
+}).listen(8124);
+console.log('Server running on 8124');
