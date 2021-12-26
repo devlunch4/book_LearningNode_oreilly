ba1519b51e327ce074dac1daebe143389a3cc42b
devlunch4@E470 <devlunch4@gmail.com>
2021-12-26 22:02:12 +0900

    change file name and fixed code.

--- ch01/hellowordl.js
+++ ch01/hellowordl.js
@@ -4,7 +4,7 @@ var http = require('http');
 // http server create - http 서버 생성
 http.createServer(function (req, res) {
     // content Header - 컨텐츠 헤ej
-    res.writable(200, { 'content-type': 'text/pain' });
+    res.writeHead(200, { 'content-type': 'text/pain' });
     // Write a message and send a signal that communication is complete - 메시지를 쓰고 통신이 완료되었다는 신호를 보냄
     res.end("Hello, World!\n");
 }).listen(8124);
